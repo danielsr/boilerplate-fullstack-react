2ff2dbfc02d02235d7bcaa444acfd638e887fb01
danielrodrigues <danielsr@gmail.com>
2018-02-17 14:09:12 -0200

    test setup and reactstrap

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,12 +3,13 @@ import React, { Component } from 'react'
 class Button extends Component {
   render() {
     return (
-			<button type={this.props.type || 'button'}
-				className={`button ${this.props.className}` || ''}
-				onClick={this.props.onClick}
-			>
-				{this.props.text}
-			</button>
+      <button
+        type={this.props.type || 'button'}
+        className={`button ${this.props.className}` || ''}
+        onClick={this.props.onClick}
+      >
+        {this.props.text}...
+      </button>
     )
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './style/style.css'
-import App from './containers/App'
+import App from './modules/App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
