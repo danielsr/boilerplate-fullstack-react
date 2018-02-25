87541703ff6ebeed50ff2d04056d99f13b9d25b3
Daniel Rodrigues <danielsr@gmail.com>
2018-02-25 12:22:54 -0300

    Adding Apollo Client

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 import { Provider } from 'react-redux'
+import { ApolloProvider } from 'react-apollo'
 import { store, history } from 'state'
+import client from 'state/apollo'
 import PrivateRoute from 'core/components/PrivateRoute'
 import Login from '../modules/Main/Login'
 import Main from '../modules/Main'
@@ -11,13 +13,15 @@ class Routes extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div>
-            <Route path="/login" component={Login} />
-            <Route exact path="/" component={Main} />
-            <Route exact path="/admin" component={Main} />
-          </div>
-        </ConnectedRouter>
+        <ApolloProvider client={client}>
+          <ConnectedRouter history={history}>
+            <div>
+              <Route path="/login" component={Login} />
+              <Route exact path="/" component={Main} />
+              <Route exact path="/admin" component={Main} />
+            </div>
+          </ConnectedRouter>
+        </ApolloProvider>
       </Provider>
     )
   }
